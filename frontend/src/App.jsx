94915da0ee5d3bd6a1cc6e7766547b46258948cb
ithar <ithar3333@gmail.com>
2024-02-17 11:47:16 +0100

    Admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
 import Contact from './Pages/Contact/Contact';
 import Creator from './Pages/creator/creator';
+//import Admin from './Pages/Admin/admin';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/create" element={<Creator/>}/>
+          <Route path='/dashboard' element={<Admin />} />
         </Routes>
       </BrowserRouter>
     </div>
