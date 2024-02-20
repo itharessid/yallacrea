ff3f122065b1f2cd8dd9da42095c5464cbf62aea
ithar <ithar3333@gmail.com>
2024-02-20 17:28:53 +0100

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react';
 import About from './pages/About/apropos';
-import Home from'./pages/Home/acceuil';
+import Home from './pages/Home/acceuil';
 import Contact from './pages/Contact/contac';
+import Formation from './pages/Formation/formation';
 import Headers from './Components/Headers';
-import  Footer from './Components/footer';
+import Footer from './Components/footer';
+import Sidbar from './pages/Admin/sidbar/Adminsidbar';
+import Dashbord from './pages/Admin/dashbord';
 
-
-import{BrowserRouter, Routes, Route} from "react-router-dom";
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   
-   <>
-  <Headers/>
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Home/>}></Route>
-    <Route path="/about" element={<About/>}></Route>
-    <Route path="/contac" element={<Contact/>}></Route>
-   </Routes>
-   </BrowserRouter>
-   <Footer/>
-   </>
-     
-  
-  )
+    <BrowserRouter>
+      <Headers />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/formation" element={<Formation/>} />
+        <Route path="/sidbar" element={<Sidbar />} />
+        <Route path="/dashbord" element={<Dashbord />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
